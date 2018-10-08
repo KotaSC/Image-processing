181a67b578e28860380d917ddcaad8ec4b5e9d4c
Kota Kawakami <is0302ke@gmail.com>
2018-10-08 16:45:59 +0900

    Add files via upload

--- calc_imageStatistics.py
+++ calc_imageStatistics.py
@@ -4,7 +4,7 @@ import numpy as np
 def main():
     
     # 入力画像を読み込み
-    img = cv2.imread( "th0.3_cropUpside.bmp" )
+    img = cv2.imread( "th0.3_dim2_cropUpside.bmp" )
 
     # グレイスケール変換
     gray = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
@@ -15,51 +15,14 @@ def main():
     sumPixelValue = np.sum( gray )   
 
     # 画素値の平均値を求める
-    # avePixelValue = np.mean( gray )
-    avePixelValue, num = calc_avePixelValue( gray, sumPixelValue, height, width )
- 
+    avePixelValue = gray[gray != 0].mean()
+    
     # 画素値の分散を求める
-    # varPixelValue = np.var( gray )
-    varPixelValue = calc_varPixelValue( gray, avePixelValue, height, width, num ) 
+    varPixelValue = gray[gray != 0].var()
 
-
-    print( "numPixel      = " + str( num ) )
     print( "sumPixelValue = " + str( sumPixelValue ) )
     print( "avePixelValue = " + str( avePixelValue ) )
     print( "varPixelValue = " + str( varPixelValue ) )
-
-# 画素値の平均を求める関数
-def calc_avePixelValue( gray_img, total, h, w ):
-
-    # 画素値が0でないピクセルの個数
-    numPixel = 0
-     
-    for y in range( h ):
-        for x in range( w ):
-            
-            # 画素値が0でない場合
-            if gray_img[ y, x ] > 0:
-                
-                numPixel += 1
-
-    return total / numPixel, numPixel
-
-# 画素値の分散を求める関数
-def calc_varPixelValue( gray_img, ave, h, w, n ):
-
-    var = 0
-    tmp = 0
-    
-    for y in range( h ):
-        for x in range( w ):
-
-             # 画素値が0でない場合
-            if gray_img[ y, x ] > 0:
-            
-                tmp = gray_img[ y, x ] - ave
-                var += np.power( tmp, 2 )
-
-    return var / n
 
 if __name__ == "__main__":
     main()
