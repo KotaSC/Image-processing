3221390dc63882ac32e129c19a989dcff4274c37
Kota Kawakami <is0302ke@gmail.com>
2018-10-14 17:44:48 +0900

    Add files via upload

--- calc_imageStatistics.py
+++ calc_imageStatistics.py
@@ -4,15 +4,13 @@ import numpy as np
 def main():
     
     # 入力画像を読み込み
-    img = cv2.imread( "th0.3_dim2_cropUpside.bmp" )
+    img = cv2.imread( "th0.3_cropUpside.bmp" )
 
     # グレイスケール変換
     gray = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
-
-    height, width = gray.shape[0], gray.shape[1]
     
     # 画素値の合計を求める
-    sumPixelValue = np.sum( gray )   
+    sumPixelValue = np.sum( gray[gray != 0] )   
 
     # 画素値の平均値を求める
     avePixelValue = gray[gray != 0].mean()
@@ -20,9 +18,13 @@ def main():
     # 画素値の分散を求める
     varPixelValue = gray[gray != 0].var()
 
+    # 画素値の標準偏差を求める
+    stdPixelValue = gray[gray != 0].std()
+
     print( "sumPixelValue = " + str( sumPixelValue ) )
     print( "avePixelValue = " + str( avePixelValue ) )
     print( "varPixelValue = " + str( varPixelValue ) )
+    print( "stdPixelValue = " + str( stdPixelValue ) )
 
 if __name__ == "__main__":
     main()
