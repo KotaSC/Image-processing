ebfd1b7ace77cfeb991d129f0e61c3e29fef1718
Kota Kawakami <is0302ke@gmail.com>
2018-10-16 16:59:10 +0900

    Add files via upload

--- thin_eval.py
+++ thin_eval.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 def main():
     
     # 入力画像を読み込み
-    img = cv2.imread( "th0.3_dim3_cropDownside.bmp" )
+    img = cv2.imread( "th0.3_dim2_cropUpside.bmp" )
     
     # グレースケール変換
     gray_img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
@@ -22,34 +22,33 @@ def main():
     plt.xlim( [0, height] )
     plt.ylim( [0, 255] )
 
+    plt.plot( val, color='b' )
+
     plt.axhline( y=m, color='r', linestyle='--' )
 
-    interSectionA = 24.2
-    interSectionB = 37.5
+    interSectionA = 20.5
+    interSectionB = 41.5
     length        = round( interSectionB - interSectionA, 1 )
     center        = ( interSectionA + interSectionB ) * 0.5
 
-    plt.plot( interSectionA, m, 'o', color='g' )
-    plt.text( interSectionA+1, m-12, str(interSectionA) )
+    plt.plot( interSectionA, m, 'o', color='black' )
+    plt.annotate( str(interSectionA), xy=(interSectionA, m), 
+									  xytext=(interSectionA-10, m+40), 
+									  arrowprops=dict( arrowstyle="simple", connectionstyle="arc3" ) )
 
-    plt.plot( interSectionB, m, 'o', color='g' )
-    plt.text( interSectionB-5, m-12, str(interSectionB) )
+    plt.plot( interSectionB, m, 'o', color='black' )
+    plt.annotate( str(interSectionB), xy=(interSectionB, m), 
+									  xytext=(interSectionB+5, m+40), 
+									  arrowprops=dict( arrowstyle="simple", connectionstyle="arc3" ) )
 
-    plt.text( center-5, m+42, 'length = ' + str(length) )
-    # plt.plot( [interSectionA+0.2, interSectionB-0.2], [m+7, m+7], color='g' )
-    # plt.plot( [interSectionA+0.2, interSectionA+0.2], [m, m+14],  color='g' )
-    # plt.plot( [interSectionB-0.2, interSectionB-0.2], [m, m+14],  color='g' )
-    plt.annotate( '', xy=(center, m), xytext=(center, m+40), arrowprops=dict(shrink=0.1) )
-
+    plt.text( center-6, m+5, 'length = ' + str(length) )
     plt.quiver( center, m, interSectionA-center+0.5, 0, angles='xy', scale_units='xy', scale=1, color='r' )
     plt.quiver( center, m, interSectionB-center-0.5, 0, angles='xy', scale_units='xy', scale=1, color='r' )
-
-    plt.plot( val, color='b' )
 
     plt.xlabel( "y-coordinate of image" )
     plt.ylabel( "Pixel Value" )
 
-    plt.savefig( "th0.3_dim3_down_hw.png" )
+    plt.savefig( "th0.3_dim2_up_hw.png" )
 
     plt.show()
 
