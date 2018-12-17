f3f6f4075e7e75ec14e7e8cbfd3dd01e8ecd0a8f
Kota Kawakami <is0302ke@gmail.com>
2018-12-17 13:25:31 +0900

    change

--- graph.py
+++ graph.py
@@ -1,33 +1,48 @@
 import numpy as np
+import sys
+args = sys.argv
 import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+from matplotlib import cycler
 
-th   = 0.3/1.0
-dim  = 3.0
-grad = 1.0/((1.0-th)**dim)
-
-
-def multi_func(x):
+def multi_func(x, th, dim, grad):
 
     condlist = [x <= th , (x > th) & (x <= 1)]
     funclist = [lambda a: 0, lambda a: grad*((a-th)**dim)]
 
     return np.piecewise(x, condlist, funclist)
 
+colors = cycler('color', ['#EE6666', '#3388BB', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
+plt.rc('axes', facecolor='#E6E6E6', edgecolor='none', axisbelow=True, grid=True, prop_cycle=colors)
+plt.rc('grid', color='w', linestyle='solid')
+plt.rc('xtick', direction='out', color='black')
+plt.rc('ytick', direction='out', color='black')
+plt.rc('patch', edgecolor='#E6E6E6')
+plt.rc('lines', linewidth=2)
+
 plt.figure(figsize=(10, 8))
+ax = plt.axes(facecolor='#E6E6E6')
+ax.set_axisbelow(True)
 
-plt.xlim([0,1])
-plt.ylim([0,1])
+ax.set_xlim([0, 1])
+ax.set_ylim([0, 1])
 
-plt.xlabel("Feature value F")
-plt.ylabel("Probability P")
-
-plt.grid(which='major',color='black',linestyle='-')
+ax.set_title("Histgram of Pixel Value of Gray Scale Image", fontsize=12)
+ax.set_xlabel("Feature value F", fontsize=12, color='black')
+ax.set_ylabel("Probability P", fontsize=12, color='black')
 
 x = np.linspace(0, 1, 100)
-y = multi_func(x)
+
+th    = input('閾値を入力してください         >>')
+ftMax = input('特徴量の最大値を入力してください >>')
+dim   = input('関数の次元を入力してください    >>')
+grad = 1.0/((1.0-float(th)/float(ftMax))**float(dim))
+
+y = multi_func(x, float(th), float(dim), grad)
 
 plt.plot(x,y)
 
-plt.savefig("nakajimake/graph/dim3.png")
+fig_name = args[1]
+plt.savefig(fig_name)
 
 plt.show()
