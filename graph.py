9499b5395c225604f35df40b3acd26c8cb9a219c
Kota Kawakami <is0302ke@gmail.com>
2019-05-03 11:22:17 +0900

    update

--- graph.py
+++ graph.py
@@ -13,17 +13,21 @@ def multi_func(x, th, dim, grad, fm):
     return np.piecewise(x, condlist, funclist)
 
 colors = cycler('color', ['#EE6666', '#3388BB', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
-plt.rc('axes', facecolor='#E6E6E6', edgecolor='none', axisbelow=True, grid=True, prop_cycle=colors)
-plt.rc('grid', color='w', linestyle='solid')
-plt.rc('xtick', direction='out', color='black')
-plt.rc('ytick', direction='out', color='black')
+plt.rc('axes',  facecolor='#E6E6E6', edgecolor='black', axisbelow=True, grid=False, prop_cycle=colors)
+plt.rc('grid',  color='w', linestyle='solid')
+plt.rc('xtick', direction='in', color='black')
+plt.rc('ytick', direction='in', color='black')
 plt.rc('patch', edgecolor='#E6E6E6')
 plt.rc('lines', linewidth=2)
 
+plt.rcParams["xtick.major.size"] = 10
+plt.rcParams["xtick.minor.size"] = 5
+plt.rcParams["ytick.major.size"] = 10
+plt.rcParams["ytick.minor.size"] = 5
 plt.rcParams["font.size"] = 14
 
 plt.figure(figsize=(10, 8))
-ax = plt.axes(facecolor='#E6E6E6')
+ax = plt.axes(facecolor='white')
 ax.set_axisbelow(True)
 
 ax.set_xlim([0, 1])
@@ -32,7 +36,7 @@ ax.set_ylim([0, 1])
 plt.xticks([0.2, 0.4, 0.6, 0.8, 1.0])
 
 
-ax.set_title(r"Realation between Feature value $\hat{F}^{(i)}$ and Function ${\rm g}(\hat{F}^{(i)})$", fontsize=20)
+# ax.set_title(r"Realation between Feature value $\hat{F}^{(i)}$ and Function ${\rm g}(\hat{F}^{(i)})$", fontsize=20)
 ax.set_xlabel(r"Feature value $\hat{F}^{(i)}$", fontsize=20, color='black')
 ax.set_ylabel(r"Function ${\rm g}(\hat{F}^{(i)})$", fontsize=20, color='black')
 
@@ -40,14 +44,14 @@ th    = input('閾値を入力してください         >>')
 ftMax = input('特徴量の最大値を入力してください >>')
 dim   = input('関数の次元を入力してください    >>')
 
-thr = float(th)/float(ftMax)
+thr   = float(th)/float(ftMax)
 denom = 1.0 - thr
-grad = 1.0/(denom**float(dim))
-x = np.linspace(0, 1, 1000)
+grad  = 1.0/(denom**float(dim))
+x     = np.linspace(0, 1, 1000)
 
 y = multi_func(x, float(thr), float(dim), grad, float(ftMax))
 
-plt.plot(x,y)
+plt.plot(x,y, color='black')
 
 fig_name = args[1]
 plt.savefig(fig_name)
